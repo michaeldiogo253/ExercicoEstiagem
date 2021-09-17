a7b4d295f77d06482f1c6ee9bfd03765c7835193
Michael Nowtech <michaeldiogo17@outlook.com>
2021-09-17 10:31:04 -0300

    updates

--- src/Estiagem.java
+++ src/Estiagem.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Estiagem {
@@ -5,31 +6,66 @@ public class Estiagem {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int quantImoveis;
+		int quantImoveis = -1;
+		ArrayList<Residencia> listaCidades = new ArrayList<>();
 
-		while (true) {
-			try {
-				String valorDigitado;
-				System.out.println("Digite a quantidade de imoveis:");
-				valorDigitado = sc.next();
-				quantImoveis = Integer.parseInt(valorDigitado);
-				if (quantImoveis >= 0 && quantImoveis <= 1000000) {
-					break;
-				} else {
-					System.out.println("Valor invalido, digite novamente...");
+		while (quantImoveis != 0) {
+
+			while (true) {
+				try {
+					String valorDigitado;
+					System.out.println("Digite a quantidade de imoveis:");
+					valorDigitado = sc.next();
+					quantImoveis = Integer.parseInt(valorDigitado);
+					if (quantImoveis >= 0 && quantImoveis <= 1000000) {
+						break;
+					} else {
+						System.out.println("Valor invalido, digite novamente...");
+					}
+				} catch (Exception e) {
+					System.out.println("VALOR INVALIDO! Digite novamente...");
 				}
-			} catch (Exception e) {
-				System.out.println("valor invalido, digite novamente...");
 			}
+			if (quantImoveis == 0) {
+				break;
+			}
+			for (int i = quantImoveis; i > 0; i--) {
+				while (true) {
+
+					try {
+						System.out.println("Digite a quantidade de moradores da residencia:");
+						int qtdMoradores = Integer.parseInt(sc.next());
+						System.out.println("Digite a quantidade de consumo total:");
+						int qtdConsumo = Integer.parseInt(sc.next());
+
+						if ((qtdMoradores >= 1 && qtdMoradores <= 10) && (qtdConsumo >= 1 && qtdConsumo <= 200)) {
+							Residencia res = new Residencia(qtdMoradores, qtdConsumo);
+							listaCidades.add(res);
+							break;
+						} else {
+							if (qtdMoradores < 1 || qtdMoradores > 10) {
+								System.out.println("Quantidade de moradores tem de ser de 1 a 10");
+							}
+							if (qtdConsumo < 1 || qtdConsumo > 200) {
+								System.out.println("Quantidade de consumo total tem de ser de 1 a 200");
+							}
+						}
+
+					} catch (Exception e) {
+						System.out.println("VALORES INVALIDOS! Digite novamente...");
+					}
+
+				}
+
+			}
+
 		}
 
-		for (int i = quantImoveis; i > 0; i--) {
-
-			System.out.println("Digite a quantidade de moradores da residencia:");
-			String qtdMoradores = sc.next();
-			System.out.println("Digite a quantidade de consumo total:");
-			String qtdConsumo = sc.next();
-
+		int cidades = 0;
+		for (Residencia residencia : listaCidades) {
+			cidades++;
+			System.out.println("Cidade " + cidades);
+			System.out.println(residencia.toString());
 		}
 
 	}
